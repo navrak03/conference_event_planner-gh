44c9f4f2f7b45564e14944abeaf0acbc98fb271e
navrak03 <vallenthynne@gmail.com>
2024-09-18 00:12:37 -0600

    Update avSlice.js

--- conference_event_planner/src/avSlice.js
+++ conference_event_planner/src/avSlice.js
@@ -39,13 +39,13 @@ export const avSlice = createSlice({
   reducers: {
     incrementAvQuantity: (state, action) => {
         const item = state[action.payload];
-        if(item) {
+        if (item) {
             item.quantity++;
         }
     },
     decrementAvQuantity: (state, action) => {
         const item = state[action.payload];
-        if (item && item > 0){
+        if (item && item.quantity > 0) {
             item.quantity--;
         }
     },
