91dd44af38077ef34cdf5007bda8314c2c7717fb
navrak03 <vallenthynne@gmail.com>
2024-09-18 15:25:12 -0600

    Update TotalCost.jsx

--- conference_event_planner/src/TotalCost.jsx
+++ conference_event_planner/src/TotalCost.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import "./TotalCost.css";
 
 const TotalCost = ({ totalCosts, ItemsDisplay }) => {
+  
+
     const total_amount = totalCosts.venue + totalCosts.av + totalCosts.meals;
 
   return (
@@ -12,11 +14,11 @@ const TotalCost = ({ totalCosts, ItemsDisplay }) => {
         </div>
         <div>
           <h2 id="pre_fee_cost_display" className="price">
-            ${total_amount}
+           ${total_amount}
           </h2>
          
             <div className="render_items">
-                <ItemsDisplay />
+             <ItemsDisplay />
             </div>
         </div>
       </div>
